1f6b0fd25b0c47a5be86f7bb57d742abc7e731f0
Chamath Jayarathne <chamathsocial@gmail.com>
2021-03-21 18:26:57 -0400

    Reviews router online

--- index.js
+++ index.js
@@ -6,10 +6,8 @@ const methodOverride = require('method-override');
 const ejsMate = require('ejs-mate');
 const catchAsync = require('./utils/catchAsync');
 const ExpressError = require('./utils/ExpressError');
-const {joiSchemaReview} = require('./joiSchemas');
-const Review = require('./models/review');
 const campgrounds = require('./router/campgroundsRoutes');
-const Campground = require('./models/campgrounds');
+const reviews = require('./router/reviewsRoutes');
 
 app.engine('ejs', ejsMate);
 app.set('view engine', 'ejs');
@@ -27,37 +25,12 @@ mongoose.connect('mongodb://localhost:27017/YelpCampV1', {
         console.log('Error connecting to Mongoose! :::' + e);
 });
 
-const validateReview = (req, res, next) => {
-    const{error} = joiSchemaReview.validate(req.body);
-    if(error){
-        const msg = error.details.map(el => el.message).join(', ');
-        throw new ExpressError(msg, 400);
-    } else {
-        next();
-    }
-}
-
 app.use('/campgrounds', campgrounds);
+app.use('/campgrounds/:id/reviews', reviews);
 
 app.get('/', (req, res) => {
     res.render('home');    
 });
-
-app.post("/campgrounds/:id/reviews", validateReview, catchAsync(async (req, res) => {
-    const camp = await Campground.findById(req.params.id);
-    const review = new Review(req.body.review);
-    camp.reviews.push(review);
-    await review.save();
-    await camp.save();
-    res.redirect(`/campgrounds/${camp.id}`);
-}));
-
-app.delete('/campgrounds/:id/reviews/:reviewId', catchAsync(async (req, res) => {
-    const {id, reviewId} = req.params;
-    Campground.findByIdAndUpdate(id, {$pull: {reviews: reviewId}})
-    await Review.findByIdAndDelete(reviewId);
-    res.redirect(`/campgrounds/${id}`);
-}));
 
 app.all('*', (req, res, next) =>{
     next(new ExpressError('Page not found', 404));
@@ -67,7 +40,6 @@ app.use((err, req, res, next) => {
     const{statusCode = 500, message = 'Something went wrong!'} = err;
 res.status(statusCode).render('error', {err});
 });
-
 
 app.listen(3000, () => {
     console.log('Online on Port 3000');
---
+++ router/reviewsRoutes.js
@@ -0,0 +1,36 @@
+const express =  require('express');
+const router = express.Router({mergeParams: true});
+const catchAsync = require('../utils/catchAsync');
+const Review = require('../models/review');
+const {joiSchemaReview} = require('../joiSchemas');
+const Campground = require('../models/campgrounds');
+
+
+const validateReview = (req, res, next) => {
+    const{error} = joiSchemaReview.validate(req.body);
+    if(error){
+        const msg = error.details.map(el => el.message).join(', ');
+        throw new ExpressError(msg, 400);
+    } else {
+        next();
+    }
+}
+
+
+router.post("/", validateReview, catchAsync(async (req, res) => {
+    const camp = await Campground.findById(req.params.id);
+    const review = new Review(req.body.review);
+    camp.reviews.push(review);
+    await review.save();
+    await camp.save();
+    res.redirect(`/campgrounds/${camp.id}`);
+}));
+
+router.delete('/:reviewId', catchAsync(async (req, res) => {
+    const {id, reviewId} = req.params;
+    Campground.findByIdAndUpdate(id, {$pull: {reviews: reviewId}})
+    await Review.findByIdAndDelete(reviewId);
+    res.redirect(`/campgrounds/${id}`);
+}));
+
+module.exports = router;
